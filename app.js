c55900b6898323d0f6fe377a92a6aac333c41c2a
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-06-20 21:14:10 +0100

    task completed

---
+++ app.js
@@ -0,0 +1,7 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+
+var name = "Nafiu Taiwo";
+var height = "100ft";
+var country = "Nigeria"
+
+console.log(name + ", " + height + " and " + country);
